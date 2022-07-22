b0118aa6fa911d736b1afa5f02999413aa6c902c
qingyuan.cao@opay-inc.com <qingyuan.cao@opay-inc.com>
2022-07-22 15:59:44 +0800

    再次提交

--- rock-auth/src/main/java/com/rock/auth/RockAuthApplication.java
+++ rock-auth/src/main/java/com/rock/auth/RockAuthApplication.java
@@ -3,23 +3,12 @@ package com.rock.auth;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
-/**
- * 启动类
- * @author liu kai
- * @since  2020-01-03 21:39
- */
 @SpringBootApplication(scanBasePackages = "com.rock")
 @Slf4j
 public class RockAuthApplication {
-    private static ConfigurableApplicationContext ctx;
-
     public static void main(String[] args) {
-        System.setProperty("Log4jContextSelector", "org.apache.logging.log4j.core.async.AsyncLoggerContextSelector");
-        System.setProperty("log4.fileName","opay-scene-auth-center-server");
-        ctx = SpringApplication.run(RockAuthApplication.class, args);
-        log.info("spring.profiles.active:{}",ctx.getEnvironment().getActiveProfiles());
+        SpringApplication.run(RockAuthApplication.class, args);
         log.info("Boot Server started.");
     }
 }
