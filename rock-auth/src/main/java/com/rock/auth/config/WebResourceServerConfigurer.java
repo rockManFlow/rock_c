65f13390cb7a71f00101a391b67d089d08dff678
qingyuan.cao@opay-inc.com <qingyuan.cao@opay-inc.com>
2022-07-28 11:13:35 +0800

    引入JWT+标记OAUTH2

---
+++ rock-auth/src/main/java/com/rock/auth/config/WebResourceServerConfigurer.java
@@ -0,0 +1,10 @@
+package com.rock.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 需要依赖oauth2
+ */
+@Configuration
+public class WebResourceServerConfigurer{
+}
