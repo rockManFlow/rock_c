85c84f470db0c28718587e3f1951c8d54cf37090
qingyuan.cao@opay-inc.com <qingyuan.cao@opay-inc.com>
2023-12-18 16:45:40 +0800

    ffmpeg框架引入

---
+++ CMakeFiles/rock_c.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rock_c.dir/function/ffmpeg/my.c.o"
+  "CMakeFiles/rock_c.dir/function/ffmpeg/my.c.o.d"
+  "output/bin/rock_c"
+  "output/bin/rock_c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/rock_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
