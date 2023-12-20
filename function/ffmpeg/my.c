727fc2826b710e801451bfc60fa6b927fe96d6a2
qingyuan.cao@opay-inc.com <qingyuan.cao@opay-inc.com>
2023-12-20 12:04:16 +0800

    submit

--- function/ffmpeg/my.c
+++ function/ffmpeg/my.c
@@ -8,7 +8,7 @@ int main(){
     //这将注册所有的FFmpeg组件，以便在后续的操作中能够正确地使用它们。
 //    av_demuxer_iterate();
 
-    const char* video = "/Users/opayc/products/rock_c/source/test.mp4";
+    const char* video = "D:\\myProjects\\rock_c\\source\\test.mp4";
     //av格式上下文
     AVFormatContext *fmt_ctx = NULL;
     //av视频包数据
@@ -17,13 +17,13 @@ int main(){
     // 打开视频文件
     ret = avformat_open_input(&fmt_ctx, video, NULL, NULL);
     if (ret < 0) {
-        fprintf(stderr, "无法打开视频文件\n");
+        fprintf(stderr, "Not Open Video File\n");
         return ret;
     }
     // 解码视频流--获取码流信息
     ret = avformat_find_stream_info(fmt_ctx, NULL);
     if (ret < 0) {
-        fprintf(stderr, "无法找到视频流\n");
+        fprintf(stderr, "No Found Video File\n");
         return ret;
     }
     // 循环解码读取每一帧视频-或者定位文件avformat_seek_file()+av_seek_frame()
