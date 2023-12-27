60890033e473070d239dabbf543c18092009e64d
qingyuan.cao@opay-inc.com <qingyuan.cao@opay-inc.com>
2023-12-27 14:57:37 +0800

    submit

--- function/ffmpeg/voice_source_check.c
+++ function/ffmpeg/voice_source_check.c
@@ -188,6 +188,106 @@ int simplest_pcm16le_cut_singlechannel(char *url,int start_num,int dur_num){
     return 0;
 }
 
+/**
+ * 将PCM16LE双声道音频采样数据转换为WAVE格式音频数据
+ * WAVE格式音频（扩展名为“.wav”）是Windows系统中最常见的一种音频。该格式的实质就是在PCM文件的前面加了一个文件头。
+ * 本程序的函数就可以通过在PCM文件前面加一个WAVE文件头从而封装为WAVE格式音频。
+ * Convert PCM16LE raw data to WAVE format
+ * @param pcmpath      Input PCM file.
+ * @param channels     Channel number of PCM file.
+ * @param sample_rate  Sample rate of PCM file.
+ * @param wavepath     Output WAVE file.
+ */
+int simplest_pcm16le_to_wave(const char *pcmpath,int channels,int sample_rate,const char *wavepath)
+{
+
+    typedef struct WAVE_HEADER{
+        char         fccID[4];
+        unsigned   long    dwSize;
+        char         fccType[4];
+    }WAVE_HEADER;
+
+    typedef struct WAVE_FMT{
+        char         fccID[4];
+        unsigned   long       dwSize;
+        unsigned   short     wFormatTag;
+        unsigned   short     wChannels;
+        unsigned   long       dwSamplesPerSec;
+        unsigned   long       dwAvgBytesPerSec;
+        unsigned   short     wBlockAlign;
+        unsigned   short     uiBitsPerSample;
+    }WAVE_FMT;
+
+    typedef struct WAVE_DATA{
+        char       fccID[4];
+        unsigned long dwSize;
+    }WAVE_DATA;
+
+
+    if(channels==0||sample_rate==0){
+        channels = 2;
+        sample_rate = 44100;
+    }
+    int bits = 16;
+
+    WAVE_HEADER   pcmHEADER;
+    WAVE_FMT   pcmFMT;
+    WAVE_DATA   pcmDATA;
+
+    unsigned   short   m_pcmData;
+    FILE   *fp,*fpout;
+
+    fp=fopen(pcmpath, "rb");
+    if(fp == NULL) {
+        printf("open pcm file error\n");
+        return -1;
+    }
+    fpout=fopen(wavepath,   "wb+");
+    if(fpout == NULL) {
+        printf("create wav file error\n");
+        return -1;
+    }
+    //WAVE_HEADER
+    memcpy(pcmHEADER.fccID,"RIFF",strlen("RIFF"));
+    memcpy(pcmHEADER.fccType,"WAVE",strlen("WAVE"));
+    fseek(fpout,sizeof(WAVE_HEADER),1);
+    //WAVE_FMT
+    pcmFMT.dwSamplesPerSec=sample_rate;
+    pcmFMT.dwAvgBytesPerSec=pcmFMT.dwSamplesPerSec*sizeof(m_pcmData);
+    pcmFMT.uiBitsPerSample=bits;
+    memcpy(pcmFMT.fccID,"fmt ",strlen("fmt "));
+    pcmFMT.dwSize=16;
+    pcmFMT.wBlockAlign=2;
+    pcmFMT.wChannels=channels;
+    pcmFMT.wFormatTag=1;
+
+    fwrite(&pcmFMT,sizeof(WAVE_FMT),1,fpout);
+
+    //WAVE_DATA;
+    memcpy(pcmDATA.fccID,"data",strlen("data"));
+    pcmDATA.dwSize=0;
+    fseek(fpout,sizeof(WAVE_DATA),SEEK_CUR);
+
+    fread(&m_pcmData,sizeof(unsigned short),1,fp);
+    while(!feof(fp)){
+        pcmDATA.dwSize+=2;
+        fwrite(&m_pcmData,sizeof(unsigned short),1,fpout);
+        fread(&m_pcmData,sizeof(unsigned short),1,fp);
+    }
+
+    pcmHEADER.dwSize=44+pcmDATA.dwSize;
+
+    rewind(fpout);
+    fwrite(&pcmHEADER,sizeof(WAVE_HEADER),1,fpout);
+    fseek(fpout,sizeof(WAVE_FMT),SEEK_CUR);
+    fwrite(&pcmDATA,sizeof(WAVE_DATA),1,fpout);
+
+    fclose(fp);
+    fclose(fpout);
+
+    return 0;
+}
+
 int main(){
 
     /**
@@ -216,8 +316,14 @@ int main(){
     simplest_pcm16le_to_pcm8("NocturneNo2inEflat_44.1k_s16le.pcm");
 
     /**
+     * todo？？
      * 可以从PCM数据中选取一段采样值保存下来，并且输出这些采样值的数值。
      * 上述代码运行后，会把单声道PCM16LE格式的“drum.pcm”中从2360点开始的120点的数据保存成output_cut.pcm文件。
      */
     simplest_pcm16le_cut_singlechannel("drum.pcm",2360,120);
+
+    /**
+     *
+     */
+    simplest_pcm16le_to_wave("NocturneNo2inEflat_44.1k_s16le.pcm",2,44100,"output_nocturne.wav");
 }
