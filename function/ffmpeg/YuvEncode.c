6679db150fafa53d2435b1d1c1d39c0da6ea4520
qingyuan.cao@opay-inc.com <qingyuan.cao@opay-inc.com>
2024-01-30 17:11:14 +0800

    实践编码

--- function/ffmpeg/YuvEncode.c
+++ function/ffmpeg/YuvEncode.c
@@ -19,118 +19,102 @@
 
 /**
  * yuv编码成指定的文件格式
- * @param argc
- * @param argv
- * @return
  */
 
-int main()
-{
-    AVFormatContext* pFormatCtx;
-    AVOutputFormat* fmt;
-    AVStream* video_stream;
-    AVCodecContext* pCodecCtx;
-    AVCodec* pCodec;
+int main(){
+    //编码后的文件像素大小，如果设置不合适，会导致编码后的文件是乱码。只有设置合适了，才能正常显示编码后的文件
+    int in_w=256,in_h=256;                           //YUV's width and height
+    const char* out_file = "D:\\myProjects\\rock_c\\source\\test_input_1.jpg";    //Output file
 
-    uint8_t* picture_buf;
-    AVPacket pkt;
-    int y_size;
-
-    int ret=0;
-
-    int in_w=480,in_h=272;                           //YUV's width and height
-    const char* out_file = "D:\\myProjects\\rock_c\\source\\test_input.jpg";    //Output file
-
+    //这种原文件不包含编码信息，使用什么编码方式都可以，关键看你想编码成什么
     char *input_url="D:\\myProjects\\rock_c\\source\\lena_256x256_yuv420p.yuv";
     FILE *in_file = fopen(input_url, "rb+");
 
     //Method 1
-    pFormatCtx = avformat_alloc_context();
-    //Guess format
-    fmt = av_guess_format("mjpeg", NULL, NULL);
-    pFormatCtx->oformat = fmt;
-    //Output URL
-    if (avio_open(&pFormatCtx->pb,out_file, AVIO_FLAG_READ_WRITE) < 0){
+    AVFormatContext* avFormatContext = avformat_alloc_context();
+    //要编码成的格式format--输出格式
+    AVOutputFormat* avOutputFormat = av_guess_format("mjpeg", NULL, NULL);
+    //设置输出格式
+    avFormatContext->oformat = avOutputFormat;
+    //Output URL 用于打开FFmpeg的输入输出文件 1：打开成功的上下文对象 2：要打开的文件 3：打开格式 读或写 或读写
+    if (avio_open(&avFormatContext->pb,out_file, AVIO_FLAG_READ_WRITE) < 0){
         printf("Couldn't open output file.");
         return -1;
     }
 
-    //Method 2. More simple
-    //avformat_alloc_output_context2(&pFormatCtx, NULL, NULL, out_file);
-    //fmt = pFormatCtx->oformat;
+    AVCodec* avCodec = avcodec_find_encoder(avOutputFormat->video_codec);//AV_CODEC_ID_MJPEG
 
-    video_stream = avformat_new_stream(pFormatCtx, 0);
-    if (video_stream==NULL){
-        return -1;
-    }
-
-    pCodecCtx = avcodec_alloc_context3(NULL);
-    pCodecCtx->codec_id = fmt->video_codec;
+    AVCodecContext* pCodecCtx = avcodec_alloc_context3(avCodec);
+    pCodecCtx->codec_id = avOutputFormat->video_codec;
     pCodecCtx->codec_type = AVMEDIA_TYPE_VIDEO;
-    pCodecCtx->pix_fmt = AV_PIX_FMT_YUV420P;
+    pCodecCtx->pix_fmt = AV_PIX_FMT_YUVJ420P;
 
     pCodecCtx->width = in_w;
     pCodecCtx->height = in_h;
 
+    //速率 num：分子 den：分母 编码时需设置，解码时不用设置
     pCodecCtx->time_base.num = 1;
     pCodecCtx->time_base.den = 25;
-    //Output some information
-    av_dump_format(pFormatCtx, 0, out_file, 1);
-    pCodec = avcodec_find_encoder(fmt->video_codec);
-    avcodec_parameters_to_context(pCodecCtx, codecParams);
-    if (avcodec_open2(pCodecCtx, pCodec,NULL) < 0){
+
+    if (avcodec_open2(pCodecCtx, avCodec,NULL) < 0){
         printf("Could not open codec.");
         return -1;
     }
-    AVFrame* frame = av_frame_alloc();
-    //Write Header
-    if (avformat_write_header(pFormatCtx, NULL) < 0) {
-        printf("无法写入文件头部信息\n");
-        return -1;
-    }
 
-    y_size = pCodecCtx->width * pCodecCtx->height;
+    AVFrame* frame = av_frame_alloc();
+    //这些必须给的，不然编码该对象会出错
+    frame->format=pCodecCtx->pix_fmt;
+    frame->height=pCodecCtx->height;
+    frame->width=pCodecCtx->width;
+
+    // 分配内存用于保存解码后的图像数据
+    int bufferSize = av_image_get_buffer_size(pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height, 1);
+    uint8_t *picture_buf = (uint8_t *) av_malloc(bufferSize);
+    //瓜分内存
+    av_image_fill_arrays(frame->data, frame->linesize, picture_buf, pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height, 1);
+
+    AVPacket pkt;
+    int y_size = pCodecCtx->width * pCodecCtx->height;
     av_new_packet(&pkt,y_size*3);
+
     //Read YUV
     size_t size=fread(picture_buf, 1, y_size*3/2, in_file);
-    if (size<=0)
-    {
+    if (size<=0){
         printf("Could not read input file.");
         return -1;
     }
+
     frame->data[0] = picture_buf;              // Y
     frame->data[1] = picture_buf+ y_size;      // U
     frame->data[2] = picture_buf+ y_size*5/4;  // V
 
-    // 分配内存用于保存解码后的图像数据
-    int bufferSize = av_image_get_buffer_size(pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height, 1);
-    uint8_t *buffer = (uint8_t *) av_malloc(bufferSize);
-    //瓜分内存
-    av_image_fill_arrays(frame->data, frame->linesize, buffer, pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height, 1);
-
     //Encode
-    ret =avcodec_send_frame(pCodecCtx,frame);
+    int ret =avcodec_send_frame(pCodecCtx,frame);
     if(ret < 0){
-        printf("Encode Error.\n");
+        printf("Encode Error ret=%d.\n",ret);
         return -1;
     }
+
     ret=avcodec_receive_packet(pCodecCtx,&pkt);
-    if (ret==1){
-        pkt.stream_index = video_stream->index;
-        av_write_frame(pFormatCtx, &pkt);
+    if(ret < 0){
+        printf("avcodec_receive_packet Error ret=%d.\n",ret);
+        return -1;
     }
 
-    av_packet_free(&pkt);
-    //Write Trailer
-    av_write_trailer(pFormatCtx);
+    printf("write packet to file\n");
+    FILE* jpeg_file = fopen(out_file, "wb");
+    fwrite(pkt.data, 1, pkt.size, jpeg_file);
+    fclose(jpeg_file);
 
-    printf("Encode Successful.\n");
+    av_packet_free(&pkt);
 
     avcodec_close(pCodecCtx);
     av_free(frame);
     av_free(picture_buf);
-    avio_close(pFormatCtx->pb);
-    avformat_free_context(pFormatCtx);
+    avio_close(avFormatContext->pb);
+    avformat_free_context(avFormatContext);
     fclose(in_file);
+
+    printf("Encode Successful.\n");
     return 0;
 }
